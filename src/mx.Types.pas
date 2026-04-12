45bcf2fed144377365968f1f5fc955ff62ee614a
MicrotronX <yusuf.zorlu@microtronx.com>
2026-04-12 15:17:36 +0200

    v2.4.0 Build 82: Intra-project agent messaging

--- src/mx.Types.pas
+++ src/mx.Types.pas
@@ -106,7 +106,7 @@ function MxGetThreadAuth: TMxAuthResult;
 
 const
   MXAI_VERSION = '2.4.0';
-  MXAI_BUILD   = 81;  // First-Setup UX, proxy_download_url, escHtml XSS fix, netsh docs
+  MXAI_BUILD   = 82;  // Intra-project agent messaging (Spec #1964, Feature #1875): target_developer_id, scope/recent_hours on peers
   MX_KEY_PREFIX = 'mxk_';
   MXAI_PROTOCOL = '2025-11-25';
   MXAI_SCHEMA_VERSION = '1.0.0';
